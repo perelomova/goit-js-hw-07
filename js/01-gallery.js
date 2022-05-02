8d705d7580ceab73642344373f2dca447d97d6c6
Kateryna Perelomova <e.v.perelomova@gmail.com>
2022-05-02 17:29:24 +0200

    Basic Lightbox and Simple Lightbox
    Add starts files and js solutions for using Basic and Simple Lightbox gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+// console.log(createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(items) {
+  return items.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  }).join('');
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+ 
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+  instance.show();
+}
